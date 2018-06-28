8541634fbb4b8952ecda489044e6bac39d2022f5
Arnas Kromelis <arnas.kromelis@gmail.com>
2018-06-28 11:27:23 +0400

    export changes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 import Grid from './components/App';
 import reducer from './reducers/';
 
-// export default {
-//     component: Grid,
-//     reducer
-// };
-
-import { withReducer } from 'react-redux-dynamic-reducer';
-
-export default withReducer(reducer, 'Grid')(Grid);
+export default {
+    component: Grid,
+    reducer
+};
