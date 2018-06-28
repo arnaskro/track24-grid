a055c28f24babbb6ef31a98794785de3d87c102c
Arnas Kromelis <arnas.kromelis@gmail.com>
2018-06-28 09:37:33 +0400

    modified export

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 export default {
   component: require('./components/App'),
   reducer: require('./reducers/'),
+  reducers: [
+    {
+      path: "Calculator",
+      reducer: require('./reducers/CalculatorReducer')
+    }
+  ],
   name: 'Grid'
 }
